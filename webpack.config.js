b4a69587d765dfcf881425fefb6efa294fbb9047
Josselin BUILS <josselin.buils@gmail.com>
2020-11-01 18:40:07 +0100

    Simplifies Webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,6 @@
-const path = require('path');
-
 module.exports = {
   entry: {
     withIndex: './src/withIndex',
     withoutIndex: './src/withoutIndex',
-  },
-  output: {
-    path: path.resolve(__dirname, 'dist'),
   },
 };
